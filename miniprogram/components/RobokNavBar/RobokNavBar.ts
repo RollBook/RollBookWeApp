5b0871c195e7106c64026783be37a80cefc8b2fa
FAll <940313262@qq.com>
2023-02-22 10:06:38 +0800

    Initial Commit

---
+++ miniprogram/components/RobokNavBar/RobokNavBar.ts
@@ -0,0 +1,27 @@
+// components/robok-nav-bar/robok-nav-bar.ts
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  options: {
+    styleIsolation: 'shared',
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
