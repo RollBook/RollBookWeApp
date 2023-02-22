53d4a9fe1c0e5943e0f826b8218aa309b14155d7
FAll <940313262@qq.com>
2023-02-22 16:06:45 +0800

    fix: 修复了自定义tabbar闪烁以及icon激活错误

--- miniprogram/custom-tab-bar/index.ts
+++ miniprogram/custom-tab-bar/index.ts
@@ -15,9 +15,7 @@ Component({
    * 选项卡初始数据
    */
   data: {
-    selected: 0 as number,
-    color: "#737674" as string,
-    selectedColor: "#26ff8e" as string,
+    active: null as number|null,
     list: [{
       pagePath: "../index/index",
       icon:"home-o",
@@ -41,14 +39,11 @@ Component({
    * 组件的方法列表
    */
   methods: {
+    
     // 切换选项卡
-    switchTab(e:WechatMiniprogram.BaseEvent) {
-      const data:Record<string, any> = e.currentTarget.dataset
-      const url:string = data.path
-      
-      wx.switchTab({url})
-      this.setData({
-        selected: data.index as number
+    async switchTab(e:any) {
+      await wx.switchTab({
+        url:this.data.list[e.detail as number].pagePath
       })
     }
 
---
+++ miniprogram/pages/logs/logs.ts
@@ -0,0 +1,18 @@
+// logs.ts
+import { formatTime } from '../../utils/dateUtil'
+
+Page({
+  data: {
+    logs: [],
+  },
+  onLoad() {
+    this.setData({
+      logs: (wx.getStorageSync('logs') || []).map((log: string) => {
+        return {
+          date: formatTime(new Date(log)),
+          timeStamp: log
+        }
+      }),
+    })
+  },
+})
