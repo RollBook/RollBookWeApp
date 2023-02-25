4fa5229adb14b392fc41c1a70f580f74de860e4d
FAll <940313262@qq.com>
2023-02-25 13:53:29 +0800

    feat: 引入grid布局

--- miniprogram/pages/index/index.ts
+++ miniprogram/pages/index/index.ts
@@ -17,13 +17,12 @@ Page({
       })
     }
    
-    // 获取首页轮播url
+    // 获取并设置首页轮播url
+    // TODO: 本地缓存轮播图url
     this.setData({
       imgList:await (await getIndexSwiper())?.data?.data as string[]
     })
     
-    
-
   },
   getUserProfile() {
     // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
