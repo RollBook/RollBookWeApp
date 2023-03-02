dcdf072bf2ac0a60fff6758ac9e7b6e7fced0e20
FAll <940313262@qq.com>
2023-03-02 16:25:35 +0800

    feat: 完成卖家昵称及学校信息的获取

--- miniprogram/components/sell/SellerInfo/SellerInfo.ts
+++ miniprogram/components/sell/SellerInfo/SellerInfo.ts
@@ -1,5 +1,7 @@
 // components/sell/BookInfo/BookInfo.ts
 import { SellerInfo } from "../../../api/sell/types";
+
+let timer:number|undefined = undefined; // 定时器序号
 Component({
   /**
    * 组件的属性列表
@@ -24,12 +26,36 @@ Component({
    * 组件的方法列表
    */
   methods: {
-    // 设定书本信息
-    setSellerInfo(e:WechatMiniprogram.BaseEvent){
-      // 防抖
+    // 设定卖家信息
+    setSellerInfo(e:WechatMiniprogram.Input){
       console.log(e);
       
-      
+      // 防抖
+      if(timer) {
+        clearTimeout(timer)
+      }
+      timer = setTimeout(()=>{
+        // 初始化临时对象
+        let temp:SellerInfo; 
+        temp = this.data.sellerInfo
+        // 修改临时对象
+        switch(e.currentTarget.id) {
+          case 'nickName': {
+            temp.nickName =  e.detail.value
+            break;
+          }
+          case "school": {
+            temp.school = e.detail.value
+            break;
+          }
+        }
+        // 赋值给卖家信息
+        this.setData({
+          sellerInfo:temp
+        })
+        console.log(this.data.sellerInfo);
+        
+      },500)
     }
   }
 })
