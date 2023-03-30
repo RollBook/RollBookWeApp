e3633127669d296b29256e152d99b037b9b6d465
FAll <940313262@qq.com>
2023-03-30 15:54:20 +0800

    feat: 完成upload请求鉴权

--- miniprogram/pages/sell/statepattern/concretestate/statebookinfo.ts
+++ miniprogram/pages/sell/statepattern/concretestate/statebookinfo.ts
@@ -1,7 +1,6 @@
 import { State } from "../state";
 import { addBooks,uploadBookImgs } from "../../../../api/sell/index";
 import { Book } from "../../../../api/sell/types";
-import { uploadFile,uploadFiles } from "../../../../utils/request/index";
 
 export default class StateBookInfo implements State{
 
@@ -50,7 +49,7 @@ export default class StateBookInfo implements State{
     .pop()?.selectComponent('#bookinfo') as WechatMiniprogram.Component.TrivialInstance;
 
     // 检查书本必填信息是否完整
-    const bookList:Book[] = this.component.data.bookList
+    const bookList:Book[] = this.component.data.bookList;
     
     let isListOk:boolean = true;
     bookList.forEach((book)=>{
@@ -62,9 +61,9 @@ export default class StateBookInfo implements State{
       
       // 根据书本statusBox初始化status
       book.statusBox.forEach(status=>{
-        book.status = book.status | parseInt(status)
-      })  
-
+        book.status = book.status | parseInt(status);
+      });
+      
     })
 
     this.component.setData({
@@ -81,19 +80,16 @@ export default class StateBookInfo implements State{
     .pop()?.selectComponent('#bookinfo') as WechatMiniprogram.Component.TrivialInstance;
   
     // 请求添加书本接口，上架书本
-    const ret = await (await addBooks(this.component.data.bookList)).data;
+    const ret = (await addBooks(this.component.data.bookList)).data;
+    
     if(ret.status === 200) {
       // 上架成功则请求上传书本图片
-      await uploadBookImgs(this.component.data.bookList);
-      wx.redirectTo({
-        url:"/pages/index/index"
-      })
-
-      setTimeout(()=>{
-        wx.showToast({
-          title:"上传成功"
+      const ret = await uploadBookImgs(this.component.data.bookList);
+      // if(ret.statusCode === 200) {
+        wx.redirectTo({
+          url:"/pages/index/index"
         })
-      },500)
+      // }
       
     } else {
       wx.showToast({
